fef499150b21f1f462cbdab3baaa05b58964a240
Harish Jagtap <harish.jagtap23@gmail.com>
2021-02-05 16:50:59 +0530

    Changed python to python3

--- mysite_fresh_start.sh
+++ mysite_fresh_start.sh
@@ -8,35 +8,14 @@ DOMAIN="harish-jagtap.com"
 DJANGO_SECRET_KEY="" |Remove this and add Secret key|
 
 # Install packages
-echo "Installing apt packages"
+echo "-------------------------------Installing apt packages-------------------------------------------"
 apt update
 apt-get install nginx
 apt-get install python3-pip
 pip3 install gunicorn
 
-# Nginx conf
-echo "Adding Nginx Configuration"
-mkdir -p $NGINX_CONF_PATH
-cp nginx.conf "${NGINX_CONF_PATH}nginx.conf"
-
-# Git clone
-mkdir -p $GIT_CLONE_PATH
-cd $GIT_CLONE_PATH
-git clone https://github.com/HarishJagtap/MyWebpage.git
-
-# MyWebpage setup
-echo "Setting up Django"
-cd "${GIT_CLONE_PATH}MyWebpage/"
-pip3 install -r requirements.txt
-cd mywebsite
-set SECRET_KEY=$DJANGO_SECRET_KEY
-python manage.py collectstatic
-python manage.py makemigrations
-python manage.py migrate --run-syncdb
-python manage.py createsuperuser
-
 # Certbot setup
-echo "Setting up Certbot"
+echo "----------------------------Setting up Certbot-------------------------------------"
 snap install core
 snap refresh core
 apt-get remove certbot
@@ -46,8 +25,29 @@ snap set certbot trust-plugin-with-root=ok
 snap install certbot-dns-route53
 certbot certonly -a manual -i nginx -d $DOMAIN,"*.${DOMAIN}"
 
+# Nginx conf
+echo "--------------------------Adding Nginx Configuration----------------------------------"
+mkdir -p $NGINX_CONF_PATH
+cp nginx.conf "${NGINX_CONF_PATH}nginx.conf"
+
+# Git clone
+mkdir -p $GIT_CLONE_PATH
+cd $GIT_CLONE_PATH
+git clone https://github.com/HarishJagtap/MyWebpage.git
+
+# MyWebpage setup
+echo "------------------------------Setting up Django---------------------------------------"
+cd "${GIT_CLONE_PATH}MyWebpage/"
+pip3 install -r requirements.txt
+cd mywebsite
+set SECRET_KEY=$DJANGO_SECRET_KEY
+python3 manage.py collectstatic
+python3 manage.py makemigrations
+python3 manage.py migrate --run-syncdb
+python3 manage.py createsuperuser
+
 # Add Startup Service
-echo "Adding Startup Service"
+echo "-----------------------------------Adding Startup Service------------------------------------"
 cd "${GIT_CLONE_PATH}MyWebpage-Implementation"
 cp mysite_start.service /etc/systemd/system/mysite_start.service
 chmod ugo+x mysite_restart.sh
